be42c53fb547620b883c6df5eece70c3f990b964
Richard Kwan <taekyung.kwan@gmail.com>
2016-10-24 16:45:04 -0400

    fix audio source files

--- bundle.js
+++ bundle.js
@@ -46746,7 +46746,7 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	var eightZeroEight = exports.eightZeroEight = ["../../../assets/samples/BD.WAV", "../../../assets/samples/SD.WAV", "../../../assets/samples/CH.WAV", "../../../assets/samples/CP.WAV", "../../../assets/samples/CB.WAV", "../../../assets/samples/CL.WAV", "../../../assets/samples/HC.WAV", "../../../assets/samples/LC.WAV"];
+	var eightZeroEight = exports.eightZeroEight = ["https://s3.amazonaws.com/react-drummachine/BD.WAV", "https://s3.amazonaws.com/react-drummachine/SD.WAV", "https://s3.amazonaws.com/react-drummachine/CH.WAV", "https://s3.amazonaws.com/react-drummachine/CP.WAV", "https://s3.amazonaws.com/react-drummachine/CB.WAV", "https://s3.amazonaws.com/react-drummachine/CL.WAV", "https://s3.amazonaws.com/react-drummachine/HC.WAV", "https://s3.amazonaws.com/react-drummachine/LC.WAV"];
 	
 	var guitarBeat = exports.guitarBeat = ["https://s3.amazonaws.com/react-drummachine/BD.WAV", "https://s3.amazonaws.com/react-drummachine/SD.WAV", "https://s3.amazonaws.com/react-drummachine/CH.WAV", "https://s3.amazonaws.com/react-drummachine/CP.WAV", "https://s3.amazonaws.com/react-drummachine/CB.WAV", "https://s3.amazonaws.com/react-drummachine/g1.wav", "https://s3.amazonaws.com/react-drummachine/g2.wav", "https://s3.amazonaws.com/react-drummachine/g3.wav"];
 
