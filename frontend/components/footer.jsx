85005536c87a2007842ff5c07265e7a65854ab4a
Richard Kwan <taekyung.kwan@gmail.com>
2016-10-25 12:27:22 -0400

    add instruction modal

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { instructionOpen: false };
-    
+    this.toggleInstructions = this.toggleInstructions.bind(this);
   }
+
+  toggleInstructions(bool){
+    this.setState({ instructionOpen: bool });
+  }
+
 
 
   render() {
     return (
       <footer className="footer">
         <div className="instructions-button footer-el">
-          <div className="instructions-text">
+          <div className="instructions-text" onClick={this.toggleInstructions.bind(this, true)}>
             Instructions
           </div>
         </div>
@@ -22,6 +28,12 @@ class Footer extends React.Component {
         <a href="https://github.com/taekyunggg/drum_machine" className="footer-github footer-el">
           <i className="fa fa-github footer-icon" aria-hidden="true"></i>
         </a>
+        <Modal
+          isOpen={this.state.instructionOpen}
+          onRequestClose={this.toggleInstructions.bind(this, false)} >
+          <div>hello</div>
+        </Modal>
+
       </footer>
     );
   }
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import Tone from 'tone';
+import Modal from 'react-modal';
 
 
 window.Tone = Tone;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  Modal.setAppElement(document.body);
   ReactDOM.render(<Root />, root);
 });
